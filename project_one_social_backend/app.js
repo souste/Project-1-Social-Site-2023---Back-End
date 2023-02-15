56ab0f9fb22a6bb6c2ca31396dbcf2fa48cc9174
Stephen <stephensouness@outlook.com>
2023-02-15 17:52:40 +0000

    allow cross origin resource sharing

---
+++ project_one_social_backend/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const postRouter = require('./routes/postRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+app.use(cors());
+
+// 1) MIDDLEWARES
+
+// will show the request info in the console
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+
+// to show requested at time in console:
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  console.log(req.headers);
+  next();
+});
+
+// 2) ROUTES
+
+app.use('/api/posts', postRouter);
+app.use('/api/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+module.exports = app;
