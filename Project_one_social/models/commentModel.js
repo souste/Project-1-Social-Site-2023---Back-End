cbd9451b4129fd34fe20c89fa28ad0202580da2e
Stephen <stephensouness@outlook.com>
2023-03-07 10:42:59 +0000

    made changes to post and comment schema

--- Project_one_social/models/commentModel.js
+++ Project_one_social/models/commentModel.js
@@ -1,19 +1,33 @@
 const mongoose = require('mongoose');
 
-const commentSchema = new mongoose.Schema({
-  comment: { type: String, required: [true, 'Comment cannot be empty'] },
-  likes: { type: Number, required: true, default: 0 },
-  createdAt: { type: Date, default: Date.now },
-  post: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'Post',
-    required: [true, 'Comment must belong to a Post'],
+const commentSchema = new mongoose.Schema(
+  {
+    comment: { type: String, required: [true, 'Comment cannot be empty'] },
+    // likes: { type: Number, required: true, default: 0 },
+    createdAt: { type: Date, default: Date.now },
+    post: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Post',
+      required: [true, 'Comment must belong to a Post'],
+    },
+    author: { type: String, required: true },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      // required: [true, 'Comment must belong to a User'],
+    },
   },
-  user: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'User',
-    required: [true, 'Comment must belong to a User'],
-  },
+  { toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
+
+commentSchema.virtual('created').get(function () {
+  return this.createdAt.toLocaleDateString('en-GB', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    timeZone: 'Europe/London',
+  });
 });
 
 commentSchema.pre(/^find/, function (next) {
--- Project_one_social/models/postModel.js
+++ Project_one_social/models/postModel.js
@@ -6,11 +6,21 @@ const postSchema = new mongoose.Schema(
     img: { type: String, required: true, unique: true },
     description: { type: String, required: true, unique: true },
     likes: { type: Number, default: 0, required: true },
-    author: mongoose.Schema.Types.ObjectId,
+    author: { type: String, required: true },
     createdAt: { type: Date, default: Date.now() },
   },
   { toJSON: { virtuals: true }, toObject: { virtuals: true } } // need this to be able to connect reviews to posts!
 );
+
+postSchema.virtual('created').get(function () {
+  return this.createdAt.toLocaleDateString('en-GB', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    timeZone: 'Europe/London',
+  });
+});
 
 postSchema.virtual('comments', {
   ref: 'Comment',
